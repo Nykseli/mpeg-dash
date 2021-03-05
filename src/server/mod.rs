0a5037c3735958fcd5dfd971df6bc87f3c8ee215
Miika Alikirri <miika.alik@gmail.com>
2021-03-05 16:53:08 +0200

    Added more improvements to the server
    Handle the following errors
      - 408 Request Timeout
      - 413 Payload Too Large (>= 4096 bytes)

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -3,23 +3,97 @@ use std::fs;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::sync::Arc;
+use std::time::Duration;
 
 use crate::config;
 use mpeg_dash::ThreadPool;
 
+const MAX_REQUEST_SIZE: usize = 4096;
+
+/// Is the last 4 bytes the end of the http header
+/// TODO: may not be usable if support for POST requests are added
+fn is_end_of_header(buffer: &[u8]) -> bool {
+    if buffer.len() < 4 {
+        return false;
+    }
+
+    // HTTP standard defines http header end as "\r\n\r\n"
+    let end = &['\r' as u8, '\n' as u8, '\r' as u8, '\n' as u8];
+    let mut temp_buf = &buffer[..];
+    while !temp_buf.is_empty() {
+        if temp_buf.ends_with(end) {
+            return true;
+        }
+        temp_buf = &temp_buf[..temp_buf.len() - 1];
+    }
+
+    false
+    // This could be used if we could make sure that the buffer doesn't contain any body data
+    // The method above is actually only really slower IF there is body data
+    // buffer[buffer.len() - 4..buffer.len()] == end
+}
+
+/// 404 File not found
 fn response_404(mut stream: SslStream<TcpStream>) {
     stream
         .write("HTTP/1.1 404 NOT FOUND\r\n\r\n".as_bytes())
         .unwrap();
 }
 
+/// 408 Request Timeout
+fn response_408(mut stream: SslStream<TcpStream>) {
+    stream
+        .write("HTTP/1.1 408 REQUEST TIMEOUT\r\n\r\n".as_bytes())
+        .unwrap();
+}
+
+/// 413 Payload Too Large
+fn response_413(mut stream: SslStream<TcpStream>) {
+    stream
+        .write("HTTP/1.1 413 PAYLOAD TOO LARGE\r\n\r\n".as_bytes())
+        .unwrap();
+}
+
 fn handle_client(mut stream: SslStream<TcpStream>) {
     let config = config::GlobalConfig::config();
 
-    // TODO: dynamic size
-    let mut buf = [0 as u8; 1024];
-    // TODO: handle Err
-    stream.read(&mut buf).unwrap();
+    // SslStream doesn't have a timeout so we need to set it to the underlying TcpStream
+    stream
+        .get_ref()
+        .set_read_timeout(Some(Duration::from_secs_f64(
+            config.performance.connection_timeout,
+        )))
+        .unwrap();
+
+    // TODO: is there more optimal way of reading?
+    let mut buf = vec![];
+    loop {
+        // TODO: why this doesn't work with vec![]?
+        //       with ./test_client.py this recieves data_len == 0 with vec![]
+        //let mut buf2 = vec![];
+        let mut temp_buf = [0 as u8; MAX_REQUEST_SIZE];
+        // TODO: should we use ssl_read for better error info?
+        match stream.read(&mut temp_buf) {
+            Ok(data_len) => {
+                buf.extend_from_slice(&temp_buf[..data_len]);
+
+                if data_len == 0 {
+                    // Not completely sure if this even ever happens
+                    break;
+                } else if is_end_of_header(&buf[..]) {
+                    break;
+                } else if buf.len() >= MAX_REQUEST_SIZE {
+                    response_413(stream);
+                    return;
+                }
+            }
+            Err(_) => {
+                // TODO: what other errors there might be?
+                response_408(stream);
+                return;
+            }
+        }
+    }
 
     // TODO: is lossy a good (fast) option?
     let request_full = String::from_utf8_lossy(&buf);
@@ -60,12 +134,16 @@ fn handle_client(mut stream: SslStream<TcpStream>) {
     };
 
     // TODO: handle Err
-    // let out = format!("HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nContent-type: {}\r\nContent-Length: {}\r\n\r\n", file_type, file_data.len());
+    // TODO: should all the responses contain information about the server? version number etc?
     let access_origin = &config.network.allow_origin[..];
     let out = format!("HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: {}\r\nContent-type: {}\r\nContent-Length: {}\r\n\r\n", access_origin, file_type, file_data.len());
     stream.write(out.as_bytes()).unwrap();
     stream.write_all(&file_data[..]).unwrap();
     stream.flush().unwrap();
+    // TODO: this should happen on every error.
+    //       create struct out of the stream that implements drop
+    // TODO:: actully do we even need this because of write_all?
+    //stream.shutdown().unwrap();
 }
 
 pub struct DashServer {
